bbe57f8e87ee483508590de4a51b579b650ee535
Xi Chen <kevinchen@me.com>
2017-09-08 16:20:44 +0200

    Introduce glue code for view models and views

---
+++ Examples/Shared/Observable.swift
@@ -0,0 +1,36 @@
+//
+//  Observable.swift
+//  TinyPlayerDemo
+//
+//  Created by Xi Chen on 08.09.17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+public class Observable<T> {
+    public typealias Observer = (T) -> Void
+    private var observers = [UUID: Observer]()
+
+    private(set) var value: T {
+        didSet {
+            observers.forEach {
+                $0.value(value)
+            }
+        }
+    }
+
+    public init(_ value: T) {
+        self.value = value
+    }
+
+    public func next(_ value: T) {
+        self.value = value
+    }
+
+    public func observe(_ observer: @escaping Observer) {
+        let id = UUID()
+        observers[id] = observer
+        observer(value)
+    }
+}
